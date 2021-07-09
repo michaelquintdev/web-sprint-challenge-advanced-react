9f92c702c47b518d600d399127afb5e0f6667e63
michaelquintdev <michaelquintdev@gmail.com>
2021-07-09 13:39:22 -0500

    Done with useForm and plantlist

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,27 @@
+import { useState } from 'react'
 // write your custom hook here to control your checkout form
+
+const initialValue = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+  };
+
+export const useForm = () => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+  
+    const handleChanges = (e) => {
+      setValues({ ...values, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setShowSuccessMessage(true);
+    };
+  
+    return ([showSuccessMessage, values, handleChanges, handleSubmit])
+  }
